902188b81548308a919e2b96f01c0de1c4a8e855
Francisco Júnior <109768198+professorfrancisco@users.noreply.github.com>
2024-03-20 19:49:16 -0300

    Update update_produto.sql

--- update_produto.sql
+++ update_produto.sql
@@ -1,4 +1,4 @@
-update produto set VALOR_COMPRA = 1.25 and VALOR_VENDA = 2.5 and QTD_ESTOQUE = 50 where id = 1;
-update produto set VALOR_COMPRA = 1 and VALOR_VENDA = 2 and QTD_ESTOQUE = 100 where id = 2;
-update produto set VALOR_COMPRA = 3 and VALOR_VENDA = 5.5 and QTD_ESTOQUE = 70 where id = 3;
-update produto set VALOR_COMPRA = 2.5 and VALOR_VENDA = 4 and QTD_ESTOQUE = 90 where id = 4;
+update produto set VALOR_COMPRA = 1.25 , VALOR_VENDA = 2.5 , QTD_ESTOQUE = 50 where id = 1;
+update produto set VALOR_COMPRA = 1 , VALOR_VENDA = 2 , QTD_ESTOQUE = 100 where id = 2;
+update produto set VALOR_COMPRA = 3 , VALOR_VENDA = 5.5 , QTD_ESTOQUE = 70 where id = 3;
+update produto set VALOR_COMPRA = 2.5 , VALOR_VENDA = 4 , QTD_ESTOQUE = 90 where id = 4;
