f90616b52a83bcea0b2b1bbb0547556cdc672578
Francisco Júnior <109768198+professorfrancisco@users.noreply.github.com>
2024-03-20 19:40:52 -0300

    Create update_produto.sql
    Update na tabela produto

---
+++ update_produto.sql
@@ -0,0 +1,4 @@
+update produto set VALOR_COMPRA = 1.25 and VALOR_VENDA = 2.5 and QTD_ESTOQUE = 50 where id = 1;
+update produto set VALOR_COMPRA = 1 and VALOR_VENDA = 2 and QTD_ESTOQUE = 100 where id = 2;
+update produto set VALOR_COMPRA = 3 and VALOR_VENDA = 5.5 and QTD_ESTOQUE = 70 where id = 3;
+update produto set VALOR_COMPRA = 2.5 and VALOR_VENDA = 4 and QTD_ESTOQUE = 90 where id = 4;
